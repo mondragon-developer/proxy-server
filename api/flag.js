34b137d67c51bf709d7485a1288b3ed12d0a90db
Jose Mondragon <121974632+mondragon-developer@users.noreply.github.com>
2025-07-16 14:24:02 -0400

    making server request with empty body

--- api/flag.js
+++ api/flag.js
@@ -16,11 +16,11 @@ module.exports = async (req, res) => {
 
     const response = await axios.post(
       awsUrl,
-      "Ramp",
+      {},
       {
-        headers: { 'Content-Type': 'application/json',
-        // Bypassing the AWS server's "bot" detection.
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        headers: { 
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
          },
       }
     );
